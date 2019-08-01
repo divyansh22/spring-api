10e2ff0691ff000a8de62cfecdd41434414f98fb
DIVYANSH JAIN <32432336+divyansh22@users.noreply.github.com>
2019-08-01 11:25:58 +0530

    Update EmployeeController.java

--- src/main/java/com/wynk/controllers/EmployeeController.java
+++ src/main/java/com/wynk/controllers/EmployeeController.java
@@ -3,7 +3,7 @@ package com.wynk.controllers;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
+
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
